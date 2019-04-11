92aeb33df3ddd8d37a499e31fdde4eb9e06c709a
Leonie Dong <leoniedong@hotmail.com>
2019-04-11 22:42:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
       holiday.each do |holiday, supply|
         if supply.include?("BBQ") == true
-          array << holiday
+          array << holiday.keys
           puts array
         end
     end
