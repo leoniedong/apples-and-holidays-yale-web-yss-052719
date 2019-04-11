54e30440347a1dd504a94a80f34a702e15798045
Leonie Dong <leoniedong@hotmail.com>
2019-04-11 22:32:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
       puts "#{season.capitalize}:"
       holiday.each do |holiday, supplies|
-        holiday.to_s
+        holiday = holiday.to_s
         puts "#{holiday.to_s.split.capitalize.join}: #{supplies.join}"
       end
   end
